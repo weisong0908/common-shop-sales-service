030289c124271c2dfd0b08da5e249244dc03b93c
Teng Wei Song <weisong.teng.work@gmail.com>
2021-01-16 15:07:59 +0800

    display swagger page on staging too

--- Startup.cs
+++ Startup.cs
@@ -48,7 +48,7 @@ namespace CommonShop.SalesService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
